d534d951ead5efdbc30f466e48adf090bfa96eb3
CodeAX2 <jacobhofer1@gmail.com>
2023-01-30 15:08:37 -0700

    Add base files for Assignment 1

---
+++ Assignment1/src/IMP.java
@@ -0,0 +1,491 @@
+/*
+ *Hunter Lloyd
+ * Copyrite.......I wrote, ask permission if you want to use it outside of
+ *class.
+ */
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.MemoryImageSource;
+import java.awt.image.PixelGrabber;
+import java.io.File;
+import java.util.prefs.Preferences;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+class IMP implements MouseListener {
+	JFrame frame;
+	JPanel mp;
+	JButton start;
+	JScrollPane scroll;
+	JMenuItem openItem, exitItem, resetItem;
+	Toolkit toolkit;
+	File pic;
+	ImageIcon img;
+	int colorX, colorY;
+	int[] pixels;
+	int[] results;
+	// Instance Fields you will be using below
+
+	// This will be your height and width of your 2d array
+	int height = 0, width = 0;
+	int firstHeight = 0, firstWidth = 0;
+
+	// your 2D array of pixels
+	int picture[][];
+
+	/**
+	 * In the Constructor I set up the GUI, the frame the menus. The open
+	 * pulldown menu is how you will open an image to manipulate.
+	 */
+	IMP() {
+		toolkit = Toolkit.getDefaultToolkit();
+		frame = new JFrame("Image Processing Software by Hunter");
+		JMenuBar bar = new JMenuBar();
+		JMenu file = new JMenu("File");
+		JMenu functions = getFunctions();
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent ev) {
+				quit();
+			}
+		});
+		openItem = new JMenuItem("Open");
+		openItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				handleOpen();
+			}
+		});
+		resetItem = new JMenuItem("Reset");
+		resetItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				reset();
+			}
+		});
+		exitItem = new JMenuItem("Exit");
+		exitItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				quit();
+			}
+		});
+		file.add(openItem);
+		file.add(resetItem);
+		file.add(exitItem);
+		bar.add(file);
+		bar.add(functions);
+		frame.setSize(600, 600);
+		mp = new JPanel();
+		mp.setBackground(new Color(0, 0, 0));
+		scroll = new JScrollPane(mp);
+		frame.getContentPane().add(scroll, BorderLayout.CENTER);
+		JPanel butPanel = new JPanel();
+		butPanel.setBackground(Color.black);
+		start = new JButton("start");
+		start.setEnabled(false);
+		start.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				fun1();
+			}
+		});
+		butPanel.add(start);
+		frame.getContentPane().add(butPanel, BorderLayout.SOUTH);
+		frame.setJMenuBar(bar);
+		frame.setVisible(true);
+	}
+
+	/**
+	 * This method creates the pulldown menu and sets up listeners to selection
+	 * of the menu choices. If the listeners are activated they call the methods
+	 * for handling the choice, fun1, fun2, fun3, fun4, etc. etc.
+	 */
+
+	private JMenu getFunctions() {
+		JMenu fun = new JMenu("Functions");
+
+		JMenuItem firstItem = new JMenuItem("MyExample - fun1 method");
+		firstItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				fun1();
+			}
+		});
+		fun.add(firstItem);
+
+		JMenuItem secondItem = new JMenuItem("Rotate 90");
+		secondItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				rotate90();
+			}
+		});
+		fun.add(secondItem);
+
+		JMenuItem thirdItem = new JMenuItem("Make Grayscale");
+		thirdItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				makeGrayscale();
+			}
+		});
+		fun.add(thirdItem);
+
+		JMenuItem fourthItem = new JMenuItem("Grayscale Blur");
+		fourthItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				grayscaleBlur();
+			}
+		});
+		fun.add(fourthItem);
+
+		JMenuItem fifthItem = new JMenuItem("Edge Mask");
+		fifthItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				edgeDetection();
+			}
+		});
+		fun.add(fifthItem);
+
+		return fun;
+	}
+
+	/**
+	 * This method handles opening an image file, breaking down the picture to a
+	 * one-dimensional array and then drawing the image on the frame.
+	 * You don't need to worry about this method.
+	 */
+	private void handleOpen() {
+		img = new ImageIcon();
+		JFileChooser chooser = new JFileChooser();
+		Preferences pref = Preferences.userNodeForPackage(IMP.class);
+		String path = pref.get("DEFAULT_PATH", "");
+
+		chooser.setCurrentDirectory(new File(path));
+		int option = chooser.showOpenDialog(frame);
+
+		if (option == JFileChooser.APPROVE_OPTION) {
+			pic = chooser.getSelectedFile();
+			pref.put("DEFAULT_PATH", pic.getAbsolutePath());
+			img = new ImageIcon(pic.getPath());
+		}
+		width = img.getIconWidth();
+		height = img.getIconHeight();
+		firstWidth = width;
+		firstHeight = height;
+
+		JLabel label = new JLabel(img);
+		label.addMouseListener(this);
+		pixels = new int[width * height];
+
+		results = new int[width * height];
+
+		Image image = img.getImage();
+
+		PixelGrabber pg =
+			new PixelGrabber(image, 0, 0, width, height, pixels, 0, width);
+		try {
+			pg.grabPixels();
+		} catch (InterruptedException e) {
+			System.err.println("Interrupted waiting for pixels");
+			return;
+		}
+		for (int i = 0; i < width * height; i++)
+			results[i] = pixels[i];
+		turnTwoDimensional();
+		mp.removeAll();
+		mp.add(label);
+
+		mp.revalidate();
+	}
+
+	/**
+	 * The libraries in Java give a one dimensional array of RGB values for an
+	 * image, I thought a 2-Dimensional array would be more usefull to you
+	 * So this method changes the one dimensional array to a two-dimensional.
+	 */
+	private void turnTwoDimensional() {
+		picture = new int[height][width];
+		for (int i = 0; i < height; i++)
+			for (int j = 0; j < width; j++)
+				picture[i][j] = pixels[i * width + j];
+	}
+
+	/**
+	 * This method takes the picture back to the original picture
+	 */
+	private void reset() {
+		height = firstHeight;
+		width = firstWidth;
+		for (int i = 0; i < width * height; i++)
+			pixels[i] = results[i];
+		Image img2 = toolkit.createImage(
+			new MemoryImageSource(width, height, pixels, 0, width)
+		);
+
+		JLabel label2 = new JLabel(new ImageIcon(img2));
+		mp.removeAll();
+		mp.add(label2);
+
+		mp.revalidate();
+		mp.repaint();
+		turnTwoDimensional();
+	}
+
+	/**
+	 * This method is called to redraw the screen with the new image.
+	 */
+	private void resetPicture() {
+		for (int i = 0; i < height; i++)
+			for (int j = 0; j < width; j++)
+				pixels[i * width + j] = picture[i][j];
+		Image img2 = toolkit.createImage(
+			new MemoryImageSource(width, height, pixels, 0, width)
+		);
+
+		JLabel label2 = new JLabel(new ImageIcon(img2));
+		mp.removeAll();
+		mp.add(label2);
+
+		mp.revalidate();
+		mp.repaint();
+	}
+
+	/**
+	 * This method takes a single integer value and breaks it down doing bit
+	 * manipulation to 4 individual int values for A, R, G, and B values
+	 */
+	private int[] getPixelArray(int pixel) {
+		int temp[] = new int[4];
+		temp[0] = (pixel >> 24) & 0xff;
+		temp[1] = (pixel >> 16) & 0xff;
+		temp[2] = (pixel >> 8) & 0xff;
+		temp[3] = (pixel)&0xff;
+		return temp;
+	}
+
+	/**
+	 * This method takes an array of size 4 and combines the first 8 bits of
+	 * each to create one integer.
+	 */
+	private int getPixels(int rgb[]) {
+		int alpha = 0;
+		int rgba = (rgb[0] << 24) | (rgb[1] << 16) | (rgb[2] << 8) | rgb[3];
+		return rgba;
+	}
+
+	public void getValue() {
+		int pix = picture[colorY][colorX];
+		int temp[] = getPixelArray(pix);
+		System.out.println(
+			"Color value " + temp[0] + " " + temp[1] + " " + temp[2] + " " +
+			temp[3]
+		);
+	}
+
+	/**************************************************************************************************
+	 * This is where you will put your methods. Every method below is called
+	 *when the corresponding pulldown menu is used. As long as you have a
+	 *picture open first the when your fun1, fun2, fun....etc method is called
+	 *you will have a 2D array called picture that is holding each pixel from
+	 *your picture.
+	 *************************************************************************************************/
+	/**
+	 * Example function that just removes all red values from the picture.
+	 * Each pixel value in picture[i][j] holds an integer value. You need to
+	 * send that pixel to getPixelArray the method which will return a 4 element
+	 * array that holds A,R,G,B values. Ignore [0], that's the Alpha channel
+	 * which is transparency, we won't be using that, but you can on your own.
+	 * getPixelArray will breaks down your single int to 4 ints so you can
+	 * manipulate the values for each level of R, G, B.
+	 * After you make changes and do your calculations to your pixel values the
+	 * getPixels method will put the 4 values in your ARGB array back into a
+	 * single integer value so you can give it back to the program and display
+	 * the new picture.
+	 */
+	private void fun1() {
+
+		for (int i = 0; i < height; i++)
+			for (int j = 0; j < width; j++) {
+				int rgbArray[] = new int[4];
+
+				// get three ints for R, G and B
+				rgbArray = getPixelArray(picture[i][j]);
+
+				rgbArray[1] = 0;
+				// take three ints for R, G, B and put them back into a single
+				// int
+				picture[i][j] = getPixels(rgbArray);
+			}
+		resetPicture();
+	}
+
+	/**
+	 * Rotates the image by 90 degrees and redraws it
+	 */
+	private void rotate90() {
+
+		// Flip width/height
+		int oldWidth = width, oldHeight = height;
+		width = oldHeight;
+		height = oldWidth;
+
+		int[][] newPicture = new int[height][width];
+
+		for (int row = 0; row < oldHeight; row++) {
+			for (int col = 0; col < oldWidth; col++) {
+				int mappedRow = height - col - 1;
+				int mappedCol = row;
+				newPicture[mappedRow][mappedCol] = picture[row][col];
+			}
+		}
+
+		picture = newPicture;
+		resetPicture();
+	}
+
+	/**
+	 * Turns the image to grayscale using the luminosity algorithm
+	 */
+	private void makeGrayscale() {
+		for (int row = 0; row < height; row++) {
+			for (int col = 0; col < width; col++) {
+				int[] argb = getPixelArray(picture[row][col]);
+				int val =
+					(int)(0.21 * argb[1] + 0.72 * argb[2] + 0.07 * argb[3]);
+				argb[1] = val;
+				argb[2] = val;
+				argb[3] = val;
+				picture[row][col] = getPixels(argb);
+			}
+		}
+		resetPicture();
+	}
+
+	/**
+	 * Turns the image to grayscale and performs a basic blur
+	 */
+	private void grayscaleBlur() {
+		makeGrayscale();
+
+		int[][] newPicture = new int[height][width];
+		for (int row = 0; row < height; row++) {
+			for (int col = 0; col < width; col++) {
+				float total = 0;
+				float numSummed = 0;
+				for (int offsetX = -1; offsetX <= 1; offsetX++) {
+					for (int offsetY = -1; offsetY <= 1; offsetY++) {
+						if (row + offsetY >= 0 && row + offsetY < height &&
+							col + offsetX >= 0 && col + offsetX < width) {
+							total += getPixelArray(
+								picture[row + offsetY][col + offsetX]
+							)[1];
+							numSummed++;
+						}
+					}
+				}
+				int average = (int)(total / numSummed);
+				newPicture[row][col] =
+					getPixels(new int[] {255, average, average, average});
+			}
+		}
+		picture = newPicture;
+		resetPicture();
+	}
+
+	/**
+	 * Turns the image grayscale and performs edge detection using a 5x5 mask
+	 */
+	private void edgeDetection() {
+		makeGrayscale();
+
+		int[][] mask = {
+			{-1, -1, -1, -1, -1},
+			{-1, 0, 0, 0, -1},
+			{-1, 0, 16, 0, -1},
+			{-1, 0, 0, 0, -1},
+			{-1, -1, -1, -1, -1}};
+
+		int[][] newPicture = new int[height][width];
+		for (int row = 0; row < height; row++) {
+			for (int col = 0; col < width; col++) {
+				int newValue = 0;
+
+				for (int maskY = 0; maskY < mask.length; maskY++) {
+					for (int maskX = 0; maskX < mask[maskY].length; maskX++) {
+						int offsetY = -(mask.length / 2) + maskY;
+						int offsetX = -(mask[maskY].length / 2) + maskX;
+
+						if (row + offsetY >= 0 && row + offsetY < height &&
+							col + offsetX >= 0 && col + offsetX < width) {
+							newValue +=
+								mask[maskY][maskX] *
+								getPixelArray(
+									picture[row + offsetY][col + offsetX]
+								)[1];
+						}
+					}
+				}
+
+				if (newValue < 50)
+					newValue = 0;
+				if (newValue >= 50)
+					newValue = 255;
+
+				newPicture[row][col] =
+					getPixels(new int[] {255, newValue, newValue, newValue});
+			}
+		}
+		picture = newPicture;
+		resetPicture();
+	}
+
+	private void quit() {
+		System.exit(0);
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent m) {}
+
+	@Override
+	public void mouseExited(MouseEvent m) {}
+
+	@Override
+	public void mouseClicked(MouseEvent m) {
+		colorX = m.getX();
+		colorY = m.getY();
+		System.out.println(colorX + "  " + colorY);
+		getValue();
+		start.setEnabled(true);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent m) {}
+
+	@Override
+	public void mouseReleased(MouseEvent m) {}
+
+	public static void main(String[] args) {
+		IMP imp = new IMP();
+	}
+}

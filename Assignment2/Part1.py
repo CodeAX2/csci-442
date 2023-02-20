8060c57d5c26115a9afdf014b1ce5f6893be2186
CodeAX2 <jacobhofer1@gmail.com>
2023-02-20 14:19:46 -0700

    Add code for assignment 2

--- Assignment2/Part1.py
+++ Assignment2/Part1.py
@@ -5,18 +5,24 @@ import numpy as np
 class ObjectTracker:
     __bgrImg: cv.Mat
     __hsvImg: cv.Mat
+    __thresholdImg: cv.Mat
 
     __capture: cv.VideoCapture
 
-    __minScalar : np.array
-    __maxScalar : np.array
+    __minScalar: np.array
+    __maxScalar: np.array
+
+    __erosionSize: int = 0
+    __dilationSize: int = 0
 
     def __init__(self) -> None:
         self.__capture = cv.VideoCapture(0)
         # Create Windows
         cv.namedWindow("BGR")
         cv.namedWindow("HSV")
+        cv.namedWindow("Threshold")
         cv.namedWindow("Trackbars")
+        cv.resizeWindow("Trackbars", 250, 400)
 
         # Add events
         cv.setMouseCallback("BGR", self.videoCallback)
@@ -68,16 +74,52 @@ class ObjectTracker:
             255,
             lambda v: self.trackbarChanged(v, "maxV"),
         )
+        cv.createTrackbar(
+            "dilation",
+            "Trackbars",
+            0,
+            20,
+            lambda v: self.trackbarChanged(v, "dilation"),
+        )
+        cv.createTrackbar(
+            "erosion",
+            "Trackbars",
+            0,
+            20,
+            lambda v: self.trackbarChanged(v, "erosion"),
+        )
 
-        self.__minScalar = np.array([0,0,0])
-        self.__maxScalar = np.array([0,0,0])
+        self.__minScalar = np.array([0, 0, 0])
+        self.__maxScalar = np.array([0, 0, 0])
 
     def startLoop(self) -> None:
         while True:
             _, self.__bgrImg = self.__capture.read()
-            cv.imshow("BGR", self.__bgrImg)
+
             self.__hsvImg = cv.cvtColor(self.__bgrImg, cv.COLOR_BGR2HSV)
+            self.__thresholdImg = cv.inRange(
+                self.__hsvImg, self.__minScalar, self.__maxScalar
+            )
+
+            dilateElement = cv.getStructuringElement(
+                cv.MORPH_RECT,
+                (2 * self.__dilationSize + 1, 2 * self.__dilationSize + 1),
+                (self.__dilationSize, self.__dilationSize),
+            )
+
+            erodeElement = cv.getStructuringElement(
+                cv.MORPH_RECT,
+                (2 * self.__erosionSize + 1, 2 * self.__erosionSize + 1),
+                (self.__erosionSize, self.__erosionSize),
+            )
+
+            self.__thresholdImg = cv.dilate(self.__thresholdImg, dilateElement)
+            self.__thresholdImg = cv.erode(self.__thresholdImg, erodeElement)
+
+            cv.imshow("BGR", self.__bgrImg)
             cv.imshow("HSV", self.__hsvImg)
+            cv.imshow("Threshold", self.__thresholdImg)
+
             k: int = cv.waitKey(1)
             if k == 27:
                 break
@@ -96,11 +138,7 @@ class ObjectTracker:
                 "BGR at (" + str(x) + ", " + str(y) + "): " + str(self.__bgrImg[y][x])
             )
 
-    """
-    Test
-    """
     def trackbarChanged(self, value: int, trackbarName: str) -> None:
-
         # Update the corresponding element of the min/max scalar
         if trackbarName == "minH":
             self.__minScalar[0] = value
@@ -114,6 +152,10 @@ class ObjectTracker:
             self.__minScalar[2] = value
         elif trackbarName == "maxV":
             self.__maxScalar[2] = value
+        elif trackbarName == "dilation":
+            self.__dilationSize = value
+        elif trackbarName == "erosion":
+            self.__erosionSize = value
 
 
 def main():
---
+++ Assignment2/Part2.py
@@ -0,0 +1,115 @@
+import cv2 as cv
+import numpy as np
+
+
+class MotionTracker:
+    __bgrImg: cv.Mat
+
+    __grayImg: cv.Mat
+    __curDst: cv.Mat
+    __capture1: cv.Mat
+    __absDiffImg: cv.Mat
+    __contourImg: cv.Mat
+
+    __capture: cv.VideoCapture
+
+    def __init__(self) -> None:
+        self.__capture = cv.VideoCapture(0)
+        # Create Windows
+        cv.namedWindow("Original")
+        cv.namedWindow("Difference")
+        cv.namedWindow("Threshold")
+        cv.namedWindow("Contours")
+
+        # Capture initial image (step 1)
+        _, self.__bgrImg = self.__capture.read()
+
+        # Create blank images (step 2)
+        self.__grayImg = np.zeros_like(self.__bgrImg)
+        self.__curDst = np.zeros_like(self.__bgrImg, dtype=np.float32)
+        self.__capture1 = np.zeros_like(self.__bgrImg)
+        self.__absDiffImg = np.zeros_like(self.__bgrImg)
+        self.__contourImg = np.zeros_like(self.__bgrImg)
+
+    def startLoop(self) -> None:
+        # Loop (step 3)
+        while True:
+            # Grab frame (step 4)
+            _, self.__bgrImg = self.__capture.read()
+
+            # Brighten image (step 4.1)
+            self.__bgrImg = cv.convertScaleAbs(self.__bgrImg, 1, 1.25)
+
+            # Copy image
+            self.__capture1 = self.__bgrImg.copy()
+
+            # Blur image (step 5)
+            blured = cv.blur(self.__capture1, (3, 3))
+
+            # Convert to 32f
+            blured = np.float32(blured)
+
+            # Accumulate Weighted (step 6)
+            self.__curDst = cv.accumulateWeighted(blured, self.__curDst, 0.5)
+
+            # Convert back to 8bit (step 7)
+            self.__capture1 = cv.convertScaleAbs(self.__curDst)
+
+            # Get abs diff (step 8)
+            self.__absDiffImg = cv.absdiff(self.__capture1, self.__bgrImg)
+
+            # Convert to grayscale (step 9)
+            self.__absDiffImg = cv.cvtColor(self.__absDiffImg, cv.COLOR_BGR2GRAY)
+
+            # Threshold (low number, step 10)
+            self.__grayImg = cv.inRange(self.__absDiffImg, 20, 255)
+
+            # Blur (step 11)
+            self.__grayImg = cv.blur(self.__grayImg, (15, 15))
+
+            # Threshold (high number, step 12)
+            self.__grayImg = cv.inRange(self.__grayImg, 120, 255)
+
+            # Find contours (step 13)
+            contours, _ = cv.findContours(
+                self.__grayImg, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE
+            )
+
+            # Make contour image white and draw contours
+            self.__contourImg.fill(255)
+
+            # Find blobs/rectangles from contours (step 14)
+            i = 0
+            for contour in contours:
+                # Cutoff for small contours
+                if contour.size <= 100:
+                    i = i + 1
+                    continue
+                # Draw contours of blobs (step 15)
+                cv.drawContours(self.__contourImg, contours, i, (0, 0, 0), -1)
+
+                # Draw rectangles (step 16)
+                x, y, w, h = cv.boundingRect(contour)
+                cv.rectangle(self.__bgrImg, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                i = i + 1
+
+            # Draw images
+            cv.imshow("Original", self.__bgrImg)
+            cv.imshow("Difference", self.__absDiffImg)
+            cv.imshow("Threshold", self.__grayImg)
+            cv.imshow("Contours", self.__contourImg)
+
+            # Exit with esc
+            k: int = cv.waitKey(1)
+            if k == 27:
+                break
+
+        cv.destroyAllWindows()
+
+
+def main():
+    tracker: MotionTracker = MotionTracker()
+    tracker.startLoop()
+
+
+main()
